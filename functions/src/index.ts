75d4e9e1a32e4212233bc78dc25297308af722d8
syntappz <doddsanater87@gmail.com>
2019-11-14 23:19:23 +0000

    init

---
+++ functions/src/index.ts
@@ -0,0 +1,48 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+admin.initializeApp();
+
+
+const vision = require("@google-cloud/vision");
+const visionClient = new vision.ImageAnnotatorClient();
+
+// Dedicated bucket for cloud function invocation
+const bucketName = "vision-camera-bucket";
+
+export const imageTagger = functions.storage
+
+  .bucket(bucketName)
+  .object()
+  .onFinalize(async (object, _context) => {
+    const filePath = object.name || "";
+
+    const imageUri = `gs://${bucketName}/${filePath}`;
+
+    const docId = filePath.split(".jpg")[0];
+
+    const docRef = admin
+      .firestore()
+      .collection("photos")
+      .doc(docId);
+
+    const results = await visionClient.labelDetection(imageUri);
+
+    const labels = results[0].labelAnnotations.map(
+      (obj: { description: string }) => obj.description
+    );
+    const food = labels.includes("Food");
+
+
+    //if labels have food check web detection results
+    if (food) {
+      const [webResult] = await visionClient.webDetection(imageUri);
+      const webDetection = webResult.webDetection;
+      const webResults = webDetection.webEntities.map(
+        (webEntity: { description: string }) => webEntity.description
+      );
+      return docRef.set({ webResults, labels });
+    } else {
+      return docRef.set({ webResults: "false" });
+    }
+  });
---
+++ src/recipeData.js
@@ -0,0 +1,37 @@
+const appId = "app_id=8d9a0a8a";
+const apiKey = "app_key=61cfb40830621f827735b64fd206f438";
+
+
+
+
+
+export function recipeData(searchQuery) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+   
+
+
+    xhr.open(
+      "GET",
+      `https://api.edamam.com/search?q=${searchQuery}&to=20`,
+      true
+    );
+    xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+
+    xhr.onload = function() {
+      if (this.status == 200) {
+
+        let data = JSON.parse(this.responseText);
+        let recipes = data.hits.map(hit => hit.recipe);
+        resolve(recipes);
+      }
+    };
+
+    xhr.onerror = function() {
+      reject('Woops, there was an error making the request.');
+      
+    };
+
+    xhr.send();
+  });
+}
