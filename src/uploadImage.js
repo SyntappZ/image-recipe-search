40ac94196ae1341a0a09c621f040d5aa23b351b2
syntappz <doddsanater87@gmail.com>
2019-11-15 17:27:27 +0000

    firestore data added

--- src/uploadImage.js
+++ src/uploadImage.js
@@ -1,68 +1,54 @@
+import * as firebase from "firebase";
 
 
 
 
-import * as firebase from 'firebase'
-import { storage } from 'firebase'
-//import db from "./firebaseInit";
+
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDiXntU5MVBNscHxgMUoNMny4ygkrkZrhM",
-    authDomain: "vision-camera-1584b.firebaseapp.com",
-    databaseURL: "https://vision-camera-1584b.firebaseio.com",
-    projectId: "vision-camera-1584b",
-    storageBucket: "vision-camera-bucket",
-    messagingSenderId: "422441599874",
-    appId: "1:422441599874:web:9ac460803a82ec4d65459b",
-    measurementId: "G-54B3SXT7FS"
-  };
-  
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyDiXntU5MVBNscHxgMUoNMny4ygkrkZrhM",
+  authDomain: "vision-camera-1584b.firebaseapp.com",
+  databaseURL: "https://vision-camera-1584b.firebaseio.com",
+  projectId: "vision-camera-1584b",
+  storageBucket: "vision-camera-bucket",
+  messagingSenderId: "422441599874",
+  appId: "1:422441599874:web:9ac460803a82ec4d65459b",
+  measurementId: "G-54B3SXT7FS"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  
+export function uploadImage(image) {
+  return new Promise((resolve, reject) => {
+    const docId = `IMAGE_ID:${Math.floor(Math.random() * 100000000000)} `;
+    const db = firebase.firestore()
     
 
+    const uploadTask = firebase
+      .storage()
+      .ref(docId)
+      .put(image);
+    
 
-  
-  export function uploadImage(image) {
-    return new Promise((resolve, reject) => {
-
-       
-
-       const docId = Math.floor(Math.random() * 1000000000).toString()
-
-       
-
-       
-       // const reg = new RegExp(image.name)
-
-        
-
-       
-
-      
-      
-    const uploadTask = firebase.storage().ref().put(image)
-    console.log()
-
- //   const photoRef = db.collection("photos").doc(path)
+    //const photoRef = db.collection("photos").doc(docId);
 
     uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, {
-        'complete': function() {
-          console.log('upload complete!');
-
-
-        }
-      });
-
-       // Create a reference to 'mountains.jpg'
-      // const imageRef = storageRef.child();
-
-
-
-       
-     resolve(image)
+      complete: () => getVisionData()
     });
-  }
+
+
+    const getVisionData = () => {
+      db.collection('photos').get().then((snapshot) => {
+       snapshot.docs.forEach(doc => {
+         if(doc.id == docId) {
+           console.log(doc)
+         }
+       })
+      })
+    
+    }
+
+    resolve(image);
+  });
+}
